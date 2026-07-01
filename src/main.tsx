dc5924ca4f95f17369ec96e9530ff9ef6d2227dc
Lucas Roman <lucasroman@gmail.com>
2026-07-01 17:26:32 -0300

    Add light/dark theme
    Changes on index.html:
    - Remove default code.
    - Add button light/dark.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.tsx'
+// import './index.css'
+import App from './App.tsx'
 // import { Greeting } from './components/Greeting.tsx'
-import { FetchResult } from './components/Fetch.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Greeting name={'Luke'} age={5} /> */}
-    <FetchResult />
   </StrictMode>,
 )
