80ec82cc37f76576d80538dc67182331c26c9480
Lucas Roman <lucasroman@gmail.com>
2026-07-10 14:18:22 -0300

    Create Fetch test

---
+++ src/__tests__/Fetch.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-libray/react';
+import '@testing-library/jest-dom';
+import FetchResult from '../components/Fetch';
+
+describe('Fetching movie', () => {
+    // 1. Contain an input field to search movie
+    // 2. It can get data from api about the movie searched
+    // 3. It can see the data obtained
+});
