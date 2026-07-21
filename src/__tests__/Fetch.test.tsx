41c96f5f62963a40747efa9e76a6700d7a4e9eec
Lucas Roman <lucasroman@gmail.com>
2026-07-21 18:40:55 -0300

    Test Fetch: add fetch tests
    Check:
    - Can get movie data from the API.
    - Can return error message if fetch fail.
    
    Remove unnecessary interface test and its imoprts.

--- src/__tests__/Fetch.test.tsx
+++ src/__tests__/Fetch.test.tsx
@@ -1,20 +1,31 @@
-import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { FetchResult } from '../components/Fetch';
+import '@testing-library/jest-dom'; // Ins't just for Jes, include Vite matchers too.
+import { fetchData } from '../components/Fetch';
+
+
+type Movie = {
+    Title: string;
+    Year: string;
+}
 
 describe('Fetching movie', () => {
 
-    it('It can input a movie on the search form', () => {
-        render(<FetchResult />);
-        
-        // find input by label
-        const input = screen.getByLabelText('Search');
+    // Check that the app is fetching data from API
+    test.only('It can fetch data from API about the movie', async () => {
+        const query = 'the matrix';
+        const url = `http://www.omdbapi.com/?t=${query}&apikey=${import.meta.env.VITE_API_KEY}`;
+        const result = await fetchData<Movie>(url);
 
-        // Simulate input typing
-        fireEvent.change(input, { target: { value: 'the matrix' } });
-
-        expect(input).toHaveValue('the matrix');
+        // Compare just a part of response (toMatchObject) with fixed data
+        expect(result).toMatchObject({ Title: 'The Matrix', Year: '1999' });
     });
-    // 2. It can get data from api about the movie searched
-    // 3. It can see the data obtained
+
+    // Check answer in case of fetch fail
+    test.only('fetchData throws on bad response', async () => {
+        // Mock fetch to return bad request
+        globalThis.fetch = vi.fn().mockResolvedValue({ ok: false });
+
+        await expect(fetchData('http://any-url.com')).rejects.toThrow('Network error');
+    })
+
+    test.todo('It can see the data obtained')
 });
