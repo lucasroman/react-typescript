e0cab2b627f84c924824219120bbc54d1a9ab1b8
Lucas Roman <lucasroman@gmail.com>
2026-07-21 18:43:47 -0300

    Change fetchData to be exportable function
    Necessary to be imported from test files.

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -14,7 +14,7 @@ type SearchFormProps = {
     setQuery: (value: string) => void;
 }
 
-async function fetchData<T>(url: string): Promise<T> {
+export async function fetchData<T>(url: string): Promise<T> {
     const res = await fetch(url);
     if (!res.ok) throw new Error('Network error');
     return res.json() as Promise<T>;
